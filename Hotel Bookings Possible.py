98c14f3cd900c622eb0a7478bf3b564c98fcf2c7
KapilJ <kapil.jain123@gmail.com>
2020-06-05 18:29:34 -0400

    misc

--- Hotel Bookings Possible.py
+++ Hotel Bookings Possible.py
@@ -5,17 +5,13 @@ class Solution:
             rooms_q = []
             zipped = zip(arrive, depart)
             arrv_dept = sorted(zipped, key=lambda x: x[0])
-            # print(arrv_dept)
-            for start in arrv_dept:
-                # print("i {},start: {},rooms_q: {},num_of_rooms: {}".format(i,start,rooms_q,num_of_rooms))
-                while rooms_q and rooms_q[0] <= start[0]:
+            for a_d in arrv_dept:
+                while rooms_q and rooms_q[0] <= a_d[0]:
                     heapq.heappop(rooms_q)
                     num_of_rooms += 1
                 if len(rooms_q) < K:
-                    heapq.heappush(rooms_q, start[1])
+                    heapq.heappush(rooms_q, a_d[1])
                     num_of_rooms -= 1
                 else:
-                    # print("i {},start: {},rooms_q: {},num_of_rooms: {}".format(
-                    #     i, start, rooms_q, num_of_rooms))
                     return False
             return True
